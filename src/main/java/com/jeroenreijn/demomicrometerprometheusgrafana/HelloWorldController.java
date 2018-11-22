aa4e053a1d2ed9438500145ce3117e796ab620c8
Jeroen Reijn <jeroen.reijn@luminis.eu>
2018-11-22 09:40:44 +0100

    Make the controller return a json object

--- src/main/java/com/jeroenreijn/demomicrometerprometheusgrafana/HelloWorldController.java
+++ src/main/java/com/jeroenreijn/demomicrometerprometheusgrafana/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.jeroenreijn.demomicrometerprometheusgrafana;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,24 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 
     @GetMapping("/")
-    public String index() {
-        return "Hello World!";
+    public ResponseEntity<Message> index() {
+        Message helloWorld = new Message("Hello World");
+        return ResponseEntity.ok(helloWorld);
+    }
+
+    class Message {
+        private String text;
+
+        public Message(String text) {
+            this.text = text;
+        }
+
+        public String getText() {
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
+        }
     }
 }
